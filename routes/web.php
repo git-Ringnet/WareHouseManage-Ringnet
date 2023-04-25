be092c92a71e2b66207ba0b5dd0a73caa638e50b
thaihieu831 <thaihieu243@gmail.com>
2023-04-25 09:49:21 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
+});
+Route::get('/data', function () {
+    return view('tables.data');
+});
+Route::get('/simple', function () {
+    return view('tables.simple');
+});
+Route::get('/jsgrid', function () {
+    return view('tables.jsgrid');
+});
+//chuyen trang
+Route::get('/{name?}', function ($name = "index") {
+    return view($name);
 });
