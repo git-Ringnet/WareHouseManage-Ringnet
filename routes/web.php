48ed8cfc216ae0cd0217995ec49d6645d705a74c
Việt Nguyễn <nqv171002@gmail.com>
2023-07-26 17:58:00 +0700

    Revert "Merge pull request #164 from git-Ringnet/tuan_merge_2.8-1"
    This reverts commit 8391dfefdb4457527ee1f28fa77b8eff1138791c, reversing
    changes made to 31a4cc89af3d2dbcb642fe29b0a0c80a3829ba26.

--- routes/web.php
+++ routes/web.php
@@ -134,15 +134,9 @@ Route::post('/insertProductP', [AddProductController::class, 'addBill'])->name('
 Route::put('/deleteBill/{id?}', [AddProductController::class, 'deleteBill'])->name('deleteBill');
 Route::get('/deleteOrder', [AddProductController::class, 'deleteOrder'])->name('deleteOrder');
 Route::get('/cancelBill', [AddProductController::class, 'cancelBill'])->name('cancelBill');
-Route::get('/accessBills', [AddProductController::class, 'accessBills'])->name('accessBills');
+Route::get('/confirmBill', [AddProductController::class, 'confirmBill'])->name('confirmBill');
 Route::get('/showProduct', [AddProductController::class, 'showProduct'])->name('showProduct');
 Route::get('/add_newProvide', [AddProductController::class, 'add_newProvide'])->name('add_newProvide');
-
-// Xóa đơn đã hủy
-Route::get('/delBillCancel',[AddProductController::class,'delBillCancel'])->name('delBillCancel');
-
-// Kiểm tra tình trạng massAction
-
 
 // Kiểm tra serial number tồn tại chưa
 Route::get('/checkSN', [AddProductController::class, 'checkSN'])->name('checkSN');
