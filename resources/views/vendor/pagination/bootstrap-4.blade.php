3e07693b31278aeb6e442b90d6f475c7900c7012
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2023-08-09 13:24:18 +0700

    push

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -8,7 +8,7 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    {{-- <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li> --}}
                 @endif
 
                 {{-- Array Of Links --}}
@@ -17,8 +17,18 @@
                         @if ($paginator->currentPage() == $page)
                             <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                         @else
-                            @if (abs($paginator->currentPage() - $page) <= 2 || $page === 1 || $page === $paginator->lastPage() || ($loop->last && $page === $paginator->lastPage() - 1)) <!-- Thêm điều kiện cho ... ở trước trang cuối cùng -->
+                            @if (
+                                $page === 1 ||
+                                $page === $paginator->lastPage() ||
+                                ($page >= $paginator->currentPage() - 2 && $page <= $paginator->currentPage() + 2)
+                            )
                                 <li class="page-item"><a class="paginate-a page-link text-dark" href="{{ $url }}">{{ $page }}</a></li>
+                            @endif
+                            @if (
+                                ($page == 2 && $paginator->currentPage() > 4) ||
+                                ($page == $paginator->lastPage() - 1 && $paginator->currentPage() < $paginator->lastPage() - 3)
+                            )
+                                <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
                             @endif
                         @endif
                     @endforeach
