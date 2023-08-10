b4599fc4639f94daf3538531906350cae7566b67
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2023-08-10 17:30:35 +0700

    commit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -625,4 +625,19 @@
                 }
             });
         });
+        $(document).ready(function() {
+            const container = document.querySelector('.scroll-custom');
+
+            function updateHeight() {
+                const windowHeight = window.innerHeight;
+                const minHeight = Math.min(100, Math.max(60, windowHeight / window.screen.height * 100));
+                container.style.maxHeight = `${minHeight}vh`;
+            }
+
+            // Set initial height
+            updateHeight();
+
+            // Update height when the window is resized
+            window.addEventListener('resize', updateHeight);
+        });
     </script>
