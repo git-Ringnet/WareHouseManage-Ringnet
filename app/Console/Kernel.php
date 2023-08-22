bdab08b94464e65ea956ef329aa976bf45b1eef3
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2023-08-22 12:22:26 +0700

    add message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,11 @@
 
 namespace App\Console;
 
+use App\Models\Debt;
+use App\Models\DebtImport;
+use App\Models\Message;
+use Carbon\Carbon;
+use DateTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +20,45 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $debtExport = Debt::all();
+            $today = Carbon::now()->format('d-m-Y');
+            foreach ($debtExport as $d) {
+                $check = date_format(new DateTime($d->date_end), 'd-m-Y');
+                if ($check == $today) {
+                    $m = Message::where('id_debtExport', $d->id)->first();
+                    if ($m === null) {
+                        $data = new Message();
+                        $data->message = 'Tới hạn thanh toán';
+                        $data->user_id = $d->user_id;
+                        $data->status = 0;
+                        $data->id_debtExport = $d->id;
+                        $data->id_debtImport = 0;
+                        $data->created_at = Carbon::now();
+                        $data->updated_at = Carbon::now();
+                        $data->save();
+                    }
+                }
+            }
+            $debtImport = DebtImport::all();
+            foreach ($debtImport as $d) {
+                $checkImport = date_format(new DateTime($d->date_end), 'd-m-Y');
+                if ($checkImport == $today) {
+                    $m = Message::where('id_debtImport', $d->id)->first();
+                    if ($m === null) {
+                        $data = new Message();
+                        $data->message = 'Tới hạn thanh toán';
+                        $data->user_id = $d->user_id;
+                        $data->status = 0;
+                        $data->id_debtImport = $d->id;
+                        $data->id_debtExport = 0;
+                        $data->created_at = Carbon::now();
+                        $data->updated_at = Carbon::now();
+                        $data->save();
+                    }
+                }
+            }
+        })->everyMinute(); // Hoặc thời gian cố định khác
     }
 
     /**
@@ -25,7 +68,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
---
+++ database/migrations/2023_08_22_111830_create_message_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->integer('user_id');
+            $table->integer('status');
+            $table->integer('id_debtImport');
+            $table->integer('id_debtExport');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+};
